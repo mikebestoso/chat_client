155619b18fbcd69f0cbb74b4777db726750ff139
mikebestoso <mikebestoso@gmail.com>
2018-12-26 15:38:07 -0500

    initial test gui
    added initial test gui code to MainWindow

--- MainWindow.py
+++ MainWindow.py
@@ -1,1 +1,15 @@
-#Placeholder for main window class
+import sys
+from PyQt5 import QtGui,QtWidgets
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QWidget()
+
+window.setGeometry(0, 0, 500, 300)
+
+window.setWindowTitle("Test GUI")
+window.setStyleSheet("background-color:blue")
+window.show()
+
+sys.exit(app.exec_())
+
+
